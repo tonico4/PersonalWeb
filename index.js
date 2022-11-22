52c513e393be0567a7dfe24c862fba3a6e2335e3
tonico4 <antogarcia.personal@gmail.com>
2022-11-22 11:32:41 +0100

    Añadir atribuciones

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const menutoggle = document.querySelector('.navtoggle');
 const btn = document.querySelector('.menubtn');
+const nav = document.querySelector('.navdiv');
 
 btn.addEventListener('click', () => {
-    menutoggle.classList.toggle('active')
+    menutoggle.classList.toggle('active');
+})
+
+window.addEventListener('scroll', () => {
+    nav.classList.toggle('fixed', window.scrollY > 0);
 })
